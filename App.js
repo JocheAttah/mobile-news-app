55e73f8051e2fdbbe378f42379a260d8a9f76a6c
Attah David 'Joche <39128641+JocheAttah@users.noreply.github.com>
2022-05-24 12:03:11 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+   marginTop: StatusBar.currentHeight,
+  },
+});
